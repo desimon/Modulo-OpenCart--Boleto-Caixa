cd6d92d18fd22a9810e56bd73522abfd04cf402d
Guilherme Desimon <guilherme@desimon.net>
2012-10-04 10:38:32 -0300

    No email HTML helper

--- catalog/language/portuguese-br/payment/boleto_cef_sigcb.php
+++ catalog/language/portuguese-br/payment/boleto_cef_sigcb.php
@@ -10,6 +10,6 @@
 // Text
 $_['text_title'] = 'Boleto CAIXA E. FEDERAL';
 $_['text_instruction'] = 'Pagamento preferencialmente nas casas lot&eacute;ricas, Ag&ecirc;ncias da Caixa ou Internet Banking.';
-$_['text_linkboleto'] = '<a href="'.HTTPS_SERVER.'index.php?route=payment/boleto_cef_sigcb/callback&order_id=%s" target="_blank">Gerar Segunda Via do Boleto</a>';
+$_['text_linkboleto'] = '<a href="'.HTTPS_SERVER.'index.php?route=payment/boleto_cef_sigcb/callback&order_id=%s" target="_blank">Gerar Segunda Via do Boleto</a> ou acesse ('.HTTPS_SERVER.'index.php?route=payment/boleto_cef_sigcb/callback&order_id=%s) para gerar a Segunda Via do Boleto';
 $_['text_payment'] = 'Seu pedido n&#227;o ser&aacute; enviado at&eacute; o recebimento da confirma&ccedil;&#227;o do pagamento.';
 ?>
